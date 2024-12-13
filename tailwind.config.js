18ff4b23da5032a03d364d9851e138e0a895c181
aary75 <101628957+aary75@users.noreply.github.com>
2024-12-13 13:52:31 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,7 @@ export default {
   theme: {
     extend: {
       colors:{
-        'primary':"#5f6FFF"
-      },
-      gridTemplateColumns:{
-        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
+        'primary':"#5F6FFF"
       }
     },
   },
